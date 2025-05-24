82618d1e55801e0589f2145b455c5fc4817f6a83
Kapil Nahariya <kkapil092@gmail.com>
2025-05-24 20:35:04 +0530

    Initial Commit

---
+++ ClickFit AI/ClickFit_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickFit_AIApp.swift
+//  ClickFit AI
+//
+//  Created by Kapil Nahariya on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ClickFit_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickFit AITests/ClickFit_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickFit_AITests.swift
+//  ClickFit AITests
+//
+//  Created by Kapil Nahariya on 24/05/25.
+//
+
+import Testing
+@testable import ClickFit_AI
+
+struct ClickFit_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
